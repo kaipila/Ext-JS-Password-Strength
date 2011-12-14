d33a9e32b0453cac27807fd7ec2ad0d8fecdae1a
Dan Harabagiu <harabagiu.dan@gmail.com>
2011-12-14 12:41:19 +0100

    Added demo page and fixed some DOM issues that were blocking stand alone page to work

---
+++ demo/demo.js
@@ -0,0 +1,57 @@
+/*global Ext : false*/
+function createForm() {
+	var form = Ext.create('Ext.form.Panel', {
+		layout			: 	'anchor',
+		frame			:	true,
+		defaultType		:	'textfield',
+		bodyStyle		:	'padding: 16px',
+		labelWidth		:	126,
+		defaults		:	{
+			msgTarget	:	'side'
+		},
+		items			:	[
+		    {
+				fieldLabel	:	'Name',
+				name		:	'name',
+				allowBlank	:	false,
+				emptyText	:	'',
+				anchor		:	'100%'
+		    },
+		    {
+				fieldLabel	:	'Password',
+				name		:	'password',
+				xtype		:	'passwordMeter',
+				anchor		:	'100%',
+				minLength	:	3,
+				allowBlank	:	false
+		    }
+		],
+	    buttons: [{
+	        text: 'Save changes',
+	        handler: function() {
+	            var form = this.up('form').getForm();
+	            if (form.isValid()) {
+					form.submit();
+	            }
+	        }
+	    }]
+	});
+	return form;
+}
+
+function createWindow() {
+	if(!Ext.getCmp("newWindow")) {
+		var newForm  = createForm();
+		var newWindow = Ext.create('Ext.window.Window', {
+			id		:	'newWindow',
+			title	:	'Test Password Meter',
+			layout	:	'fit',
+			items	:	[ newForm ]
+		});
+		newWindow.show();
+	}
+}
+
+Ext.onReady(function() {
+	createWindow();
+});
